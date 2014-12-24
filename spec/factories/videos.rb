b9aaf7dafa4c41a4c5847d891262397a04dda1de
Andy Pike <andy@andypike.com>
2014-12-24 19:30:13 +0000

    Cleaned up video factory

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -1,13 +1,10 @@
 FactoryGirl.define do
   factory :video do
-    title "MyString"
-presenter_id 1
-originally_posted_on "2014-12-22 21:42:01"
-original_url "MyString"
-hosted_by 1
-embed_url "MyString"
-status 1
-thumbnail "MyString"
+    title        "A great ruby talk"
+    original_url "http://youtube.com/12345"
+    hosted_by    "you_tube"
+    embed_url    "http://youtube.com/embed/12345"
+    status       "published"
+    originally_posted_on "2014-01-01"
   end
-
 end
